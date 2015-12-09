4ba07d242ba060edcb81226996cc54bea71c44b9
angu <angu@mikrosimage.eu>
2015-12-09 18:04:22 +0100

    [rez] Update version to rc.mikros.2.7.0

--- package.py
+++ package.py
@@ -1,9 +1,7 @@
 name = 'rez'
-version = 'mikros2.6.0'
+version = 'rc.mikros2.7.0'
 
 requires = ['python-2.7'] # for logging
 
 def commands():
     env.PYTHONPATH.append('{root}/src')
-
-    env.PATH.append('{root}/bin')
