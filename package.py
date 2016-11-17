44075e183ef22d81f54700d3c23361173666e7af
Joran Bigalet <joran.bigalet@gmail.com>
2016-11-17 17:01:06 +0100

    amail compliant package.py infos

--- package.py
+++ package.py
@@ -3,6 +3,16 @@ version = 'rc.mikros2.6.0'
 
 requires = ['python-2.7'] # for logging
 
+# Mikros specific
+custom = {
+    'description': 'Package configuration and resolution system',
+    # 'doc': 'http://docsanim.mikros.int/docs/libPkg',
+    # 'wiki': 'http://wiki.mikros.int/doku.php?id=anim:dev:libpkg',
+    'authors': ['gou', 'jbi'],
+    'maintainers': ['gou', 'jbi'],
+}
+
+
 def commands():
     env.PYTHONPATH.append('{root}/src')
 
