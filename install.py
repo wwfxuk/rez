b1735a708ad370816db107afd06c64b8d2542346
Joseph Yu <joseph.yu@wwfx.co.uk>
2018-08-20 17:13:17 +0100

    Added package style install

--- install.py
+++ install.py
@@ -17,6 +17,9 @@ sys.path.insert(0, src_path)
 
 from rez.utils._version import _rez_version
 from rez.backport.shutilwhich import which
+from rez.package_maker__ import make_package
+from rez.system import system
+from rez.utils.lint_helper import env, source
 from build_utils.virtualenv.virtualenv import Logger, create_environment, \
     path_locations
 from build_utils.distlib.scripts import ScriptMaker
@@ -101,37 +104,19 @@ def copy_completion_scripts(dest_dir):
     return None
 
 
-if __name__ == "__main__":
-    usage = ("usage: %prog [options] DEST_DIR ('{version}' in DEST_DIR will "
-             "expand to Rez version)")
-    parser = OptionParser(usage=usage)
-    parser.add_option(
-        '-v', '--verbose', action='count', dest='verbose', default=0,
-        help="Increase verbosity.")
-    parser.add_option(
-        '-s', '--keep-symlinks', action="store_true", default=False,
-        help="Don't run realpath on the passed DEST_DIR to resolve symlinks; "
-             "ie, the baked script locations may still contain symlinks")
-    opts, args = parser.parse_args()
+def install(dest_dir, verbosity=0):
+    """Install rez into the given directory.
 
-    if " " in os.path.realpath(__file__):
-        err_str = "\nThe absolute path of install.py cannot contain spaces due to setuptools limitation.\n" \
-                  "Please move installation files to another location or rename offending folder(s).\n"
-        parser.error(err_str)
+    Args:
+        dest_dir (str): Full path to the install directory.
 
-    # determine install path
-    if len(args) != 1:
-        parser.error("expected DEST_DIR")
-
-    dest_dir = args[0].format(version=_rez_version)
-    dest_dir = os.path.expanduser(dest_dir)
-    if not opts.keep_symlinks:
-        dest_dir = os.path.realpath(dest_dir)
-
+    Keyword Args:
+        verbosity (int): Level of verbosity (typically from args).
+    """
     print "installing rez to %s..." % dest_dir
 
     # make virtualenv verbose
-    log_level = Logger.level_for_integer(2 - opts.verbose)
+    log_level = Logger.level_for_integer(2 - verbosity)
     logger = Logger([(log_level, sys.stdout)])
 
     # create the virtualenv
@@ -143,7 +128,7 @@ if __name__ == "__main__":
                                          "PATHEXT":os.environ.get("PATHEXT",
                                                                   "")})
     args = [py_executable, "setup.py", "install"]
-    if opts.verbose:
+    if verbosity:
         print "running in %s: %s" % (source_path, " ".join(args))
     p = subprocess.Popen(args, cwd=source_path)
     p.wait()
@@ -176,5 +161,57 @@ if __name__ == "__main__":
             print "source {0}/complete.{1}".format(completion_path, ext)
         else:
             print "relevant completion script from:"
-        print completion_path
+            print completion_path
     print
+
+
+if __name__ == "__main__":
+    usage = ("usage: %prog [options] DEST_DIR ('{version}' in DEST_DIR will "
+             "expand to Rez version)")
+    parser = OptionParser(usage=usage)
+    parser.add_option(
+        '-v', '--verbose', action='count', dest='verbose', default=0,
+        help="Increase verbosity.")
+    parser.add_option(
+        '-p', '--as-package', dest='package',
+        help="Given a package name, install using rez package structure.")
+    parser.add_option(
+        '-s', '--keep-symlinks', action="store_true", default=False,
+        help="Don't run realpath on the passed DEST_DIR to resolve symlinks; "
+             "ie, the baked script locations may still contain symlinks")
+    opts, args = parser.parse_args()
+
+    if " " in os.path.realpath(__file__):
+        parser.error("\n"
+            "The absolute path of install.py cannot contain spaces "
+            "due to setuptools limitation.\n"
+            "Please move installation files to another location or "
+            "rename offending folder(s).\n")
+
+    # determine install path
+    if len(args) != 1:
+        parser.error("expected DEST_DIR")
+
+    dest_dir = args[0].format(version=_rez_version)
+    dest_dir = os.path.expanduser(dest_dir)
+    if not opts.keep_symlinks:
+        dest_dir = os.path.realpath(dest_dir)
+
+    if opts.package:
+        def make_root(variant, root):
+            install(root, verbosity=opts.verbose)
+
+        def commands():
+            env.PATH.append('{this.root}/bin')
+            env.PATH.append('{this.root}/bin/rez')
+            source('{this.root}/completion/complete.sh')
+
+        with make_package(opts.package, dest_dir, make_root=make_root) as pkg:
+            pkg.version = _rez_version
+            pkg.commands = commands
+            pkg.description = 'Standalone, production-ready Rez installation'
+            pkg.variants = [[
+                "platform-{0}".format(system.platform),
+                "python-{0.major}.{0.minor}".format(sys.version_info)]]
+    else:
+        install(dest_dir, verbosity=opts.verbose)
