2e2d3170cbcff02cc3dc576bce0bc03f03778780
krel <krel@mikrosimage.eu>
2019-01-07 07:51:05 +0100

    Update package custom var

--- package.py
+++ package.py
@@ -3,6 +3,11 @@ version = 'rc.2.0.0.mikros.2.0'
 
 requires = ['python-2.7'] # for logging
 
+customs = {
+    "authors": ['nerdvegas'],
+    "maintainers": ["coreTech"]
+}
+
 def commands():
     env.PYTHONPATH.append('{root}/src')
 
