5831e3d5c9cae2c2e9dc3fae0b3d9f9cb03eeca8
Joseph Yu <josephyu2712+github@gmail.com>
2020-02-23 18:59:20 +0000

    Initial working install

--- src/rez/utils/installer.py
+++ src/rez/utils/installer.py
@@ -6,6 +6,7 @@ from rez.system import system
 import os.path
 import sys
 import shutil
+import subprocess
 
 
 def install_as_rez_package(repo_path):
@@ -39,3 +40,42 @@ def install_as_rez_package(repo_path):
 
     print('')
     print("Success! Rez was installed to %s/rez/%s" % (repo_path, rez.__version__))
+
+
+def install_as_production_package(install_py, repo_path, pkg_name='rez'):
+    """Install a production rez installation as a rez package.
+
+    Note: This is very similar to 'rez-bind rez', however rez-bind is intended
+    for deprecation. Rez itself is a special case.
+
+    Args:
+        repo_path (str): Repository to install the rez package into.
+    """
+    def commands():
+        import os
+        env.PATH.append(os.path.join('{this.root}', 'bin', 'rez'))
+        env.PYTHONPATH.append(os.path.join('{this.root}', 'python'))
+
+    def make_root(variant, root):
+        subprocess.check_call(['python', install_py, root])
+
+        # copy source
+        rez_path = rez.__path__[0]
+        site_path = os.path.dirname(rez_path)
+        rezplugins_path = os.path.join(site_path, "rezplugins")
+
+        py_root = os.path.join(root, "python")
+        os.mkdir(py_root)
+        shutil.copytree(rez_path, os.path.join(py_root, "rez"))
+        shutil.copytree(rezplugins_path, os.path.join(py_root, "rezplugins"))
+
+        print('Setting up "%s"' % os.path.join(variant.base, 'package.py'))
+
+    with make_package(pkg_name, repo_path, make_root=make_root) as pkg:
+        # Production Python venv probably works for same platform + arch combo
+        pkg.variants = [system.variant[:-1]]
+        pkg.version = rez.__version__
+        pkg.commands = commands
+
+    print('SUCCESS! After activating Rez, you can then:')
+    print('rez env %s' % pkg_name)
