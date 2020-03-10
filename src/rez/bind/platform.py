0cdb7f82b93a4e08e7ce1c9873f428ed80f9fe66
Joseph Yu <joseph.yu@wwfx.co.uk>
2020-03-10 14:56:46 +0000

    Added post_commands for XDG_*

--- src/rez/bind/platform.py
+++ src/rez/bind/platform.py
@@ -12,8 +12,36 @@ def bind(path, version_range=None, opts=None, parser=None):
     version = Version(system.platform)
     check_version(version, version_range)
 
+    def post_commands():
+        """Setup default XDG_* environment variables.
+
+        https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html#variables
+        """
+        import os
+        xdg_defaults = (
+            ('XDG_DATA_HOME', ['$HOME/.local/share']),
+            ('XDG_DATA_DIRS', ['/usr/local/share', '/usr/share']),
+            ('XDG_CONFIG_HOME', ['$HOME/.config']),
+            ('XDG_CONFIG_DIRS', ['/etc/xdg']),
+            ('XDG_CACHE_HOME', ['$HOME/.cache']),
+        )
+        for xdg_var, defaults in xdg_defaults.items():
+            invalid_var = undefined(xdg_var) or not str(env[xdg_var])
+            paths = [] if invalid_var else str(env[xdg_var]).split(os.pathsep)
+            append = len(defaults) != 1
+
+            for default_path in defaults:
+                expanded = expandvars(default_path)
+                if not(default_path in paths or expanded in paths):
+                    if append:
+                        env[xdg_var].append(expanded)
+                    else:
+                        env[xdg_var] = expanded
+
     with make_package("platform", path) as pkg:
         pkg.version = version
+        if version == 'linux':
+            pkg.post_commands = post_commands
 
     return pkg.installed_variants
 
